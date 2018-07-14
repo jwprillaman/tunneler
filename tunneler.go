6c1e5f892fc813b8b38320bc49fa88122280a283
James <jwprillaman@gmail.com>
2018-07-14 16:39:08 -0600

    ability to read from the file

--- tunneler.go
+++ tunneler.go
@@ -3,12 +3,36 @@ package main
 import(
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main(){
 
 	var config = flag.String("c", "", "Configuation file to mimic.")
 	flag.Parse()
-	fmt.Printf("Using : %v\n", *config)
+	if *config == "" {
+        log.Fatal("Please provide a valid configuration file via the -c flag.")
+	} else { //continue normally
+        fmt.Printf("Using : %v\n", *config)
+        //Open config
+        configFile,err := os.Open(*config)
+        if err != nil {
+            log.Fatal(err)
+        } else {
+            //continue
+            var configFileInfo,configFileInfoErr = configFile.Stat()
+            if configFileInfoErr != nil {
+                log.Fatal(configFileInfoErr)
+            } else {
+                data := make([]byte, configFileInfo.Size())
+                count, err := configFile.Read(data)
+                if err != nil {
+                        log.Fatal(err)
+                }
+                fmt.Printf("read %d bytes: %q\nWith total size : %v\n", count, data[:count], configFileInfo.Size())
+            }
+        }
+	}
 
 }
